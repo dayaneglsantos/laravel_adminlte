2cc4842e42a35ab2a1e5c44d135da6f718e49a22
Dayane <dayaneglsantos@gmail.com>
2025-08-26 18:11:10 -0300

    register user

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,1 +1,64 @@
-Register
+@extends('layouts.auth')
+@section('body_class', 'register-page')
+
+@section('content')
+    <div class="register-box">
+        <div class="register-logo">
+            <a href="../index2.html"><b>Admin</b>LTE</a>
+        </div>
+        <!-- /.register-logo -->
+        <div class="card">
+            <div class="card-body register-card-body">
+                <p class="register-box-msg">Register a new membership</p>
+                <form action="{{ route('register') }}" method="post">
+                    @csrf
+                    <div class="input-group mb-3">
+                        <input type="text" name="name" class="form-control @error('name') is-invalid @enderror"
+                            placeholder="Full Name" value="{{ old('name') }}" />
+                        <div class="input-group-text"><span class="bi bi-person"></span></div>
+                        @error('name')
+                            <span class="invalid-feedback">
+                                {{ $message }}
+                            </span>
+                        @enderror
+                    </div>
+                    <div class="input-group mb-3">
+                        <input type="email" name="email" class="form-control @error('email') is-invalid @enderror"
+                            placeholder="Email" value="{{ old('email') }}" />
+                        <div class="input-group-text"><span class="bi bi-envelope"></span></div>
+                        @error('email')
+                            <span class="invalid-feedback">
+                                {{ $message }}
+                            </span>
+                        @enderror
+                    </div>
+                    <div class="input-group mb-3">
+                        <input type="password" name="password" class="form-control @error('password') is-invalid @enderror"
+                            placeholder="Password" />
+                        <div class="input-group-text"><span class="bi bi-lock-fill"></span></div>
+                        @error('password')
+                            <span class="invalid-feedback">
+                                {{ $message }}
+                            </span>
+                        @enderror
+                    </div>
+                    <div class="input-group mb-3">
+                        <input type="password" name="password_confirmation" class="form-control"
+                            placeholder="Password Confirmation" />
+                        <div class="input-group-text"><span class="bi bi-lock-fill"></span></div>
+                    </div>
+
+                    <div class="d-grid gap-2">
+                        <button type="submit" class="btn btn-primary">Register</button>
+                    </div>
+
+                </form>
+
+                <p class="mb-0 mt-2 text-center">
+                    <a href="login.html" class="text-center"> I already have a membership </a>
+                </p>
+            </div>
+            <!-- /.register-card-body -->
+        </div>
+    </div>
+@endsection
